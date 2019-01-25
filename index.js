4027ea09499e91cb26ac5f6592adf416cebb491f
Dewyzee <dewyzee_fe@163.com>
2019-01-25 14:21:36 +0800

    support aplipay build

--- index.js
+++ index.js
@@ -7,26 +7,35 @@
 //   return typeof global !== 'undefined' ? global : this;
 // })();
 
+const banner = `
+  if (!global && (typeof my !== 'undefined')) {
+    var globalModule = require('global');
+    var Component = globalModule.AFAppX.WorkerComponent;
+    global = globalModule.AFAppX.$global || {};
+  }
+`;
+
 function mpvueVendorPlugin() {
 }
 
-mpvueVendorPlugin.prototype.apply = function (compiler) {
-  compiler.plugin('emit', (compilation, callback) => {
-    const fileName = 'common/vendor.js';
-    const asset = compilation.assets[fileName];
-    let fileContent = asset.source();
-
-    compilation.assets[fileName] = {
-      source: () => {
-        let from = /g\s=\s\(function\(\)\s\{\r?\n?\s+return\sthis;\r?\n?\s*\}\)\(\)\;/;
-        let to = `g = (function() { return typeof global !== 'undefined' ? global : this; })();`
-        fileContent = fileContent.replace(from, to)
-        return fileContent;
-      },
-      size: () => {
-        return Buffer.byteLength(fileContent, 'utf8');
-      }
-    };
+mpvueVendorPlugin.prototype.apply = function(compiler) {
+  compiler.plugin("emit", (compilation, callback) => {
+    let regExp = /\.js$/;
+    let filesName = Object.keys(compilation.assets).filter(name =>
+      name.match(regExp)
+    );
+    filesName.forEach(name => {
+      let asset = compilation.assets[name];
+      let fileContent = asset.source();
+      compilation.assets[name] = {
+        source: () => {
+          return banner + "\n" + fileContent;
+        },
+        size: () => {
+          return Buffer.byteLength(fileContent, "utf8");
+        }
+      };
+    });
     callback();
   });
 };
